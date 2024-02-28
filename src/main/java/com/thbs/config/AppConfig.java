5498780819985e136c801b9bf42bc8d738c767b5
Sahil <74923719+notSahil@users.noreply.github.com>
2024-02-28 17:32:01 +0530

    Update AppConfig.java

--- src/main/java/com/thbs/config/AppConfig.java
+++ src/main/java/com/thbs/config/AppConfig.java
@@ -47,6 +47,7 @@ public class AppConfig {
                                 "http://localhost:3000", 
                                 "http://localhost:4000",
                                 "http://localhost:4200",
+                                "http://65.0.181.27:3000",
                                 "http://localhost:3001"
                                 
                             )
